53f892c4f394a047f18b1441165ada0e485a6f6a
Avneesh Singh <avneeshsingh@Avneeshs-MacBook-Pro.local>
2023-10-22 11:36:53 +0530

    User accounts auth and functionality added

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings.
+LOGIN_REDIRECT_URL = 'learning_logs:index'
+LOGOUT_REDIRECT_URL = 'learning_logs:index'
+LOGIN_URL = 'accounts:login'
